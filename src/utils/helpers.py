6f8fb3283113d998d022d6ed95e08ce7d73ce891
Jack-42 <johnjack0987@gmail.com>
2023-04-02 19:53:15 -0600

    Add function for top-k predictions

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,3 +1,7 @@
+import torch
+import numpy as np
+
+
 def conv2d_output_dim(dim_in: int, kernel_dim: int, stride: int = 1, padding: int = 0, dilation: int = 1) -> int:
     """
     Get output height or width for a Conv2d layer, 
@@ -10,3 +14,16 @@ def conv2d_output_dim(dim_in: int, kernel_dim: int, stride: int = 1, padding: in
     :return out, int, the output height or width of the Conv2d layer
     """
     return 1 + ((dim_in + (2 * padding) - (dilation * (kernel_dim - 1)) - 1) // stride)
+
+
+def get_top_k(model: torch.nn.Module, img: torch.Tensor, k: int = 5):
+    """
+    Get top k predictions for a given input.
+    :param model: torch.nn.Module, classifcation model
+    :param img: torch.Tensor, input whale image
+    :param k: int, number of top indices to retrieve
+    :return top k indices of model outputs
+    """
+    outputs = model(img)  # larger outputs indicate higher probability
+    top_k_indices = np.argpartition(outputs, -k)[-k:]
+    return top_k_indices
