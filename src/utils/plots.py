cf73f75374ab54acc2998e5ef1b780841e968424
John Allen Ringer <johnjack0987@gmail.com>
2023-04-06 18:54:27 -0600

    Add plots.py

---
+++ src/utils/plots.py
@@ -0,0 +1,29 @@
+"""
+@author: Jack Ringer, Mike Adams
+Date: 4/7/2023
+Description: File containing various plotting-related functions
+"""
+
+import matplotlib.pyplot as plt
+
+
+def plot_loss(epochs, train_losses, val_losses, save_pth: str):
+    plt.figure(figsize=(9, 9))
+    plt.loglog(epochs, train_losses, label="train")
+    plt.loglog(epochs, val_losses, label="val")
+    plt.legend()
+    plt.title("Model Loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.savefig(save_pth, dpi=350)
+
+
+def plot_accuracy(epochs, train_accs, val_accs, save_pth: str):
+    plt.figure(figsize=(9, 9))
+    plt.loglog(epochs, train_accs, label="train")
+    plt.loglog(epochs, val_accs, label="val")
+    plt.legend()
+    plt.title("Model Accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.savefig(save_pth, dpi=350)
