50ce2a89f332802d48dc48343238fbe84b73808e
John Allen Ringer <johnjack0987@gmail.com>
2023-04-11 19:11:50 -0600

    change loss/accuracy plots only use log scale for y-axis

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 
 def plot_loss(epochs, train_losses, val_losses, save_pth: str):
     plt.figure(figsize=(9, 9))
-    plt.loglog(epochs, train_losses, label="train")
-    plt.loglog(epochs, val_losses, label="val")
+    plt.plot(epochs, train_losses, label="train")
+    plt.plot(epochs, val_losses, label="val")
+    plt.yscale('log')
     plt.legend()
     plt.title("Model Loss")
     plt.ylabel("Loss")
@@ -20,8 +21,9 @@ def plot_loss(epochs, train_losses, val_losses, save_pth: str):
 
 def plot_accuracy(epochs, train_accs, val_accs, save_pth: str):
     plt.figure(figsize=(9, 9))
-    plt.loglog(epochs, train_accs, label="train")
-    plt.loglog(epochs, val_accs, label="val")
+    plt.plot(epochs, train_accs, label="train")
+    plt.plot(epochs, val_accs, label="val")
+    plt.yscale('log')
     plt.legend()
     plt.title("Model Accuracy")
     plt.ylabel("Accuracy")
