857ea4732c49845de9ebf68f3c93bf3f33fe4b3e
John Allen Ringer <johnjack0987@gmail.com>
2023-04-17 20:15:54 -0600

    Update twin siamese to use an additional head module

--- src/models/basic_twin_siamese.py
+++ src/models/basic_twin_siamese.py
@@ -8,16 +8,18 @@ class BasicTwinSiamese(nn.Module):
     https://datahacker.rs/019-siamese-network-in-pytorch-with-application-to-face-similarity/
     """
 
-    def __init__(self, backbone: nn.Module):
+    def __init__(self, backbone: nn.Module, head: nn.Module):
         """
         Initialize siamese network
         :param backbone: nn.Module, backbone network that will
         """
         super().__init__()
         self.backbone = backbone
+        self.head = head
 
     def forward_once(self, x):
         output = self.backbone(x)
+        output = self.head(output)
         return output
 
     def forward(self, x1, x2):
@@ -27,12 +29,15 @@ class BasicTwinSiamese(nn.Module):
 if __name__ == "__main__":
     from torchvision.models import efficientnet_b2
 
-    backbone = efficientnet_b2(num_classes=50)
-    print(backbone)
-    siam_net = BasicTwinSiamese(backbone)
+    n_features = 512
+    backbone = efficientnet_b2(num_classes=n_features)
+    head = nn.Sequential(nn.Linear(n_features, 256), nn.ReLU(inplace=True),
+                         nn.Linear(256, 128))
+    siam_net = BasicTwinSiamese(backbone, head)
     img1 = torch.randn((1, 3, 256, 256))
     img2 = torch.randn((1, 3, 256, 256))
     o1, o2 = siam_net(img1, img2)
+    print(sum(p.numel() for p in siam_net.parameters() if p.requires_grad))
     print(o1.shape)
     print(torch.max(o1))
     print(torch.min(o1))
